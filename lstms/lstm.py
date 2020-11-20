ea5b7d9f028e7307fb2878987b9f883123fd064e
David Shaw <davidshaw@Davids-MacBook-Pro-217.local>
2020-11-20 08:55:14 -0800

    make lstmswork with variable batch size and save outputs

--- lstms/lstm.py
+++ lstms/lstm.py
@@ -12,16 +12,27 @@ from matplotlib.backends.backend_pdf import PdfPages
 
 class LSTMPipeline:
 
-    def __init__(self, data_path: str, bear_id: int):
+    def __init__(self, data_path: str, bear_id: int, window_size: int):
+        """
+        Args:
+            data_path: filepath of the bear data to read in
+            bear_id: select a particular bear
+            window_size: length of the time series to model. 
+                ex. Based on the last n time steps, predict the next value
+        """
 
         self.raw_data = pd.read_csv(data_path)
         self.bear_id = bear_id
+        self.window_size = window_size
 
         if bear_id not in self.raw_data.Bear_ID:
             raise ValueError(f"Specified bear ID {self.bear_id} "
                              f"not in {self.raw_data.bear_id.unique()}")
 
-    def preprocess_stage(self) -> Tuple[MinMaxScaler, np.array, np.array]:
+        # Create a scaler object since we need it to back-transform outputs
+        self.scaler = MinMaxScaler()
+
+    def preprocess_stage(self) -> Tuple[np.array, np.array]:
         """
         Read in data, set the datetime column, and do the test/train split.
 
@@ -35,33 +46,37 @@ class LSTMPipeline:
         df_formatted.datetime = pd.to_datetime(df_formatted.datetime)
         df_formatted = df_formatted.set_index('datetime')
 
+        # Store the formatted dataset for later
+        self.data_formatted = df_formatted
+
         # Complete prototype: split naively.
         split_len = int(0.7 * len(df_formatted))
 
         train, test = df_formatted[:split_len], df_formatted[split_len:]
 
         # Normalize
-        m = MinMaxScaler()
-        train_scaled = m.fit_transform(train)
-        test_scaled = m.transform(test)
+        self.scaler.fit(train)
+        train_scaled = self.scaler.transform(train)
+        test_scaled = self.scaler.transform(test)
 
-        return m, train_scaled, test_scaled
+        return train_scaled, test_scaled
 
     def fit_model_stage(self, train: np.array) -> keras.models.Sequential:
 
         # Example taken from
         # https://medium.com/@cdabakoglu/time-series-forecasting-arima-lstm-prophet-with-python-e73a750a9887
 
-        n_input = len(train) - 1
         n_features = 1
+        batch_size = 64
+        length = self.window_size
         generator = TimeseriesGenerator(train, train,
-                                        length=n_input, batch_size=1)
-
+                                        length=length, batch_size=batch_size)
+        
         lstm_model = keras.models.Sequential()
-        lstm_model.add(layers.LSTM(4, activation='relu',
-                                   input_shape=(n_input, n_features),
-                                   batch_size=1,
-                                   stateful=True))
+        lstm_model.add(layers.LSTM(40, activation='relu',
+                                   input_shape=(None, n_features),
+                                   # batch_size=batch_size,
+                                   stateful=False))  # Stateful true for correlated long-term predictions
         lstm_model.add(layers.Dense(1))
         lstm_model.compile(optimizer='adam', loss='mse')
 
@@ -69,23 +84,59 @@ class LSTMPipeline:
         lstm_model.fit(generator, epochs=20)
         return lstm_model
 
-    def predict_model_stage(self, model, test_data) -> np.array:
+    def predict_model_stage(self, model, dataset, start_idx):
+        """ Predict out using a moving time window
 
-        outputs = model.predict(test_data)
-        return outputs
+        Args:
+        model - the fit LSTM
+        dataset - full dataset, train and test. used to feed inputs for prediction
+        start_idx- where the train/test divide index is. We want to validate against the test dataset
+        """
+
+        # Logic: for a call with window=5, and starting index of 500:
+        # we want to use the last five datapoints to predict the value at index 500
+        # then compare to index 500 at the end
+
+        outputs = []
+
+        for window_end in range(start_idx, len(dataset)):
+
+            window_start = window_end - self.window_size
+            pred_input = dataset[window_start:window_end]
+            pred_input_reshaped = pred_input.reshape((1, window, 1))
+            output = model.predict(pred_input_reshaped)
+            outputs.append(output)
+
+        # Reverse the minmax transformation
+        outputs_stacked = np.stack(outputs).reshape(len(outputs), 1)
+        outputs_unscaled = self.scaler.inverse_transform(outputs_stacked)
+        test_unscaled = self.scaler.inverse_transform(dataset)
+
+        # Plot
+        with PdfPages('bear_plot.pdf') as pdf:
+            plt.plot(outputs_unscaled, 'r-', test_unscaled[start_idx:], 'b-')
+            plt.title(f"Plot with window of {self.window_size}")
+            pdf.savefig()
+            plt.close()
+
 
     def main(self):
 
-        transform, train_data, test_data = self.preprocess_stage()
+        # Preprocessing. Train/test split, and min-max scale
+        train_data, test_data = self.preprocess_stage()
 
+        # Fit the LSTM
         model = self.fit_model_stage(train_data)
-        outputs = self.predict_model_stage(model, test_data)
 
-        return outputs
+        # Create predictions and plot. 
+        full_data = np.concatenate([train_data, test_data])
+        start_idx = len(train_data)
+        outputs = self.predict_model_stage(model, full_data, start_idx, 5)
+
 
 
 if __name__ == '__main__':
-    bear_path = '/users/shawd/Capstone/Archive/femaleclean4.csv'
+    bear_path = 'femaleclean4.csv'
     pipeline = LSTMPipeline(bear_path, 7)
 
     outputs = pipeline.main()
