d2b28bed8de2a3a772e992bcaa280e73d3053d7f
David Shaw <shawd@login02.cm.cluster>
2021-03-25 01:50:11 -0500

    remove predictions and plots

--- deep_learning/batchfile.sh
+++ deep_learning/batchfile.sh
@@ -4,7 +4,7 @@
 #SBATCH -e errorfile-%a.txt
 #SBATCH -p fp-gpgpu-3
 #SBATCH -t 60
-#SBATCH -D $1
+#SBATCH -D /users/shawd/repos/MSDS_Capstone/deep_learning
 #SBATCH --array 0-29
 
-/users/shawd/miniconda3/envs/capstone/bin/python attention.py
+/users/shawd/miniconda3/envs/capstone/bin/python multivar_lstm.py $1
